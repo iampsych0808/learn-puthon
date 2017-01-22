ebc0c4ad050a0fdad5ba31393cf094a99e482265
Kenny <Kenny>
2017-01-22 18:12:34 +0000

    First script

---
+++ Ch1.Hello_World.py
@@ -0,0 +1,12 @@
+# Chapter 1 pg 21
+# This program sayys hello ans asks for my name
+
+print('Hello World')
+print('What is your name ?')
+myName=input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge=input()
+print('You will be ' +str(int(myAge) +1) + ' in a year.')
